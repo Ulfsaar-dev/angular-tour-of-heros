ee8289fa4b06cc1bfbffe2a52c71eb327977ce35
Ulfsaar-dev <wangtoby1125@outlook.com>
2020-01-31 11:20:29 -0800

    Service Created

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heros';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
